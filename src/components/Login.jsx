d203640438ebd149321c8145767e2663db4c551e
saisivakumar-gali <galisaisivakumar@gmail.com>
2025-12-20 11:13:50 +0530

    built logout feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,6 +9,7 @@ import { BASE_URL } from '../utils/constants';
 const Login = () => {
   const [emailId,setEmailId]=useState('virat@gmail.com');
   const [password,setPassword]=useState('Virat@1234');
+  const [error,setError]=useState('');
    const dispatch=useDispatch();
    const navigate=useNavigate();
 
@@ -23,6 +24,7 @@ const Login = () => {
       navigate("/");
     }
     catch(err){
+      setError(err?.response?.data || "Something went wrong");
       console.log("Login Error:",err);
     }
   }
@@ -43,6 +45,7 @@ const Login = () => {
   <input type="text" value={password} className="input" onChange={(e)=>setPassword(e.target.value)}  />
 </fieldset>
     </div>
+    <p className='text-red-500'>{error}</p>
     <div className="card-actions justify-center m-2">
       <button className="btn btn-primary" onClick={handleLogin}>Login</button>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
+import { removeUser } from '../utils/userSlice';
 
 const Navbar = () => {
   const user=useSelector(store=>store.user);
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+
+  const handleLogout=async()=>{
+    try{
+     await axios.post(BASE_URL+"/logout",{}, {withCredentials:true});
+      dispatch(removeUser());
+      return navigate("/login");
+
+    }
+    catch(err){
+      console.log("Logout Error:",err);
+    }
+  }
   return (
     
 <div className="navbar bg-base-300 shadow-sm">
@@ -32,7 +49,7 @@ const Navbar = () => {
           </Link>
         </li>
         <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+        <li><a onClick={handleLogout}>Logout</a></li>
       </ul>
     </div>
   </div>}
